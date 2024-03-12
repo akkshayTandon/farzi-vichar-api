88aa217635bd4d52946a4bbc4fec4b2936bc3c98
akkshayat165 <76265879+akkshayTandon@users.noreply.github.com>
2024-03-13 01:12:14 +0530

    update index.js

---
+++ data/User/data.js
@@ -0,0 +1,8 @@
+/**
+ - This file is only for adding Quotes submitted by user;
+  
+ */
+
+/*  ------ DO NOT CHANGE ANYTHING IN THIS FILE. ------  */
+
+export const user_data_array = []; 
--- index.js
+++ index.js
@@ -8,21 +8,22 @@
 import express from "express";
 import 'dotenv/config';
 import cors from "cors";
+import { user_data_array } from "./data/User/data.js";
+// import { sqlite3, verbose } from "sqlite3";
 import pkg from 'sqlite3';
 
 import router from "./routes/route.js";
 import languages_array from "./languages.js";
 
 const port = process.env.PORT || 3000;
-const enviornment = process.env.NODE_ENV;
+const enviornment = process.env.NODE_ENV
 const app = express();
-
 const { sqlite3, verbose } = pkg;
 const sqlite = verbose();
 
 app.use(cors({
     origin: "*",
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'POST']
 }))
 
 app.get('/', (req, res) => {
@@ -42,7 +43,8 @@ app.get("/language-list", (req, res) => {
 });
 
 app.get("/user-data", (req, res) => {
-
+    const { sqlite3, verbose } = pkg;
+    const sqlite = verbose();
     function readDataFromDatabase(databaseFile, tableName) {
         return new Promise((resolve, reject) => {
             const db = new sqlite.Database(databaseFile, (err) => {
@@ -51,9 +53,9 @@ app.get("/user-data", (req, res) => {
                     return;
                 }
 
-                // console.log('Connected to database');
+                console.log('Connected to database');
 
-                const query = `SELECT * FROM ${tableName}`; // get all rows query
+                const query = `SELECT * FROM ${tableName}`; // Modify to select specific columns if needed
 
                 db.all(query, (err, rows) => {
                     if (err) {
@@ -64,77 +66,87 @@ app.get("/user-data", (req, res) => {
                 });
 
                 db.close(); // Close the connection after the query finishes
-
-                // console.log("Closed Database Connection");
             });
         });
     }
-
-    readDataFromDatabase("quotify.db", "user_quotes")
+    readDataFromDatabase('quotify.db', 'test_table')
         .then(data => {
-            // console.log('Data from database:', data);
-
-            res.json(data);  // Process the retrieved data here (e.g., display in a table, manipulate)
+            console.log('Data from database:', data);
+            // Process the retrieved data here (e.g., display in a table, manipulate)
+            res.json(data);
         })
         .catch(error => {
-            console.error("Error reading data:", error.message);
+            console.error('Error reading data:', error.message);
         });
+    // res.json(user_data_array);
 });
 
 app.post("/add-data", express.json(), (req, res) => {
 
     const data = req.body;
 
+    let defaultResponseData = "Data added successfully!";
+    let duplicateFlag = false;
+
+    if (user_data_array.length === 0) {
+        user_data_array.push(data);
+    } else if (user_data_array.length > 0) {
+
+        for (let i = 0; i < user_data_array.length; i++) {
+            if ((user_data_array[i].author === data.author) || (user_data_array[i].content === data.content)) {
+                duplicateFlag = true;
+                break;
+            }
+        }
+
+        duplicateFlag ? defaultResponseData = "Duplicate data, already exists!" : user_data_array.push(data)
+    }
+
+    console.log(user_data_array)
+    const { sqlite3, verbose } = pkg;
+    const sqlite = verbose();
     function writeToDatabase() {
+        const db = new sqlite.Database("./quotify.db", (err) => {
+            if (err) {
+                console.error("Error opening database:", err.message);
+                return;
+            }
+            console.log("Connected to database");
 
-        return new Promise((resolve, reject) => {
-            const db = new sqlite.Database("./quotify.db", (err) => {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-
-                const createTableQuery = `CREATE TABLE IF NOT EXISTS user_quotes (
-                author TEXT UNIQUE,
-                content TEXT UNIQUE,
+            const createTableQuery = `CREATE TABLE IF NOT EXISTS test_table (
+                author TEXT,
+                content TEXT,
                 UNIQUE (author, content)
               );`;
-
-                db.run(createTableQuery, (err) => {
-                    if (err) {
-                        reject(err);
-                        return;
-                    } else {
-                        const insertQuery = `INSERT INTO user_quotes(author, content) VALUES (?, ?)`;
-                        const values = [data.author, data.content];
-                        db.run(insertQuery, values, (err) => {
-                            if (err) {
-                                reject(err);
-                                return;
-                            } else {
-                                resolve("Data added successfully!");
-                            }
-                        });
-                    }
+            db.run(createTableQuery, (err) => {
+                if (err) {
+                    console.error("Error creating table: ", err.message);
+                    return;
+                }
+                const insertQuery = `INSERT INTO test_table(author, content) VALUES (?, ?)`;
+                user_data_array.forEach((object) => {
+                    const values = [object.author, object.content];
+                    db.run(insertQuery, ...values, (err) => {
+                        if (err) {
+                            console.error("Error inserting data", err.message);
+                        } else {
+                            console.log("Data inserted successfully");
+                        }
+                    });
                 });
+            });
 
-                db.close((err) => {
-                    if (err) {
-                        reject(err);
-                        return;
-                    }
-                    // resolve("Connection closed");
-                });
+            // Close the database connection
+            db.close((err) => {
+                if (err) {
+                    return console.error(err.message);
+                }
+                console.log("Close the database connection.");
             });
         });
     }
-
-    writeToDatabase().then(result => {
-        res.status(201).send(result);
-    }).catch(error => {
-        // console.error("Error reading data:", error.message);
-        res.status(409).send("Duplicate data, already exists!");
-    });
+    writeToDatabase();
+    duplicateFlag ? res.status(409).send(defaultResponseData) : res.status(201).send(defaultResponseData);
 });
 
 app.use("/language", router);
