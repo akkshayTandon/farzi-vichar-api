08f3ee3c20c7602ff5eedcd3598f27bbc2c7123d
akkshayat165 <76265879+akkshayTandon@users.noreply.github.com>
2024-04-04 20:37:06 +0530

    update index.js and vercel.json

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ const app = express();
 const { sqlite3, verbose } = pkg;
 const sqlite = verbose();
 
-// app.use(cors({
-//     origin: "*",
-//     methods: ['GET', 'POST']
-// }))
-app.use(cors())
+app.use(cors({
+    origin: "*",
+    methods: ['GET', 'POST']
+}))
+// app.use(cors())
 
 app.get('/', (req, res) => {
 
