783b10b36a47d5090322bd45c0ca313b9e9d2e97
akkshayat165 <76265879+akkshayTandon@users.noreply.github.com>
2024-04-04 21:25:27 +0530

    update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const { sqlite3, verbose } = pkg;
 const sqlite = verbose();
 
 app.use(cors({
-    origin: "*",
+    origin: "http://127.0.0.1:5500/test-3.html",
     methods: ['GET', 'POST']
 }))
 // app.use(cors())
